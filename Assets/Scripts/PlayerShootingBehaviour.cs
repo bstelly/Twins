8d800b0ec9d88afb0320715653ab94bbb4e89ef2
ZacharyBriggs <zacharybriggs64@gmail.com>
2018-12-11 12:27:05 -0600

    started player primary fire

---
+++ Assets/Scripts/PlayerShootingBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZachScripts
+{
+    public class PlayerShootingBehaviour : MonoBehaviour
+    {
+        public void FirePrimary(Vector3 direction)
+        {
+            this.transform.LookAt(new Vector3(direction.x,0,direction.z));
+            Debug.Log("Rotating to " + direction);
+            //have the player face that direction
+            //call the weapons fire and fire in the direction of the stick
+        }
+    }
+}
