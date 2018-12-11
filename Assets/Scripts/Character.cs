6bd52ab57a64ee3de42e6205e4cfc8a82ca62c07
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2018-12-11 11:25:44 -0600

    created Character class

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character
+{
+    public abstract string Name { get; set; }
+    public abstract float Health { get; set; }
+    public abstract float MovementSpeed { get; set; }
+    public abstract void Move(Vector3 Direction);
+    public abstract void Die();
+}
