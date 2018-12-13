58711bffbc5986b6d1c0c9cb439d7048f58c22fc
ZacharyBriggs <zacharybriggs64@gmail.com>
2018-12-13 11:34:13 -0600

    created shield ability class

---
+++ Assets/Scripts/ShieldAbility.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ZachScripts;
+
+public class ShieldAbility : Ability
+{
+    float health;
+    float size;
+    float duration;
+}
