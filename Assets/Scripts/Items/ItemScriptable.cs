a78a1b967896c54a1a81b397d4b3cf0fc35b14bc
Dylan Guidry <guidry.dylan.95@gmail.com>
2018-12-13 10:02:47 -0600

    Basic Weapon

---
+++ Assets/Scripts/Items/ItemScriptable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Items
+{
+    public enum ItemSlotType
+    {
+        Helmet, LeftArm, RightArm, LeftLeg, RightLeg, Chest
+    }
+    [CreateAssetMenu]
+    public class Item : ScriptableObject
+    {
+        public string Name;
+        public ItemSlotType SlotType;
+    }
+}
---
+++ Assets/Scripts/Items/WeaponScriptable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Items
+{    
+    public abstract class Weapon : Item
+    {
+        public float RateOfFire;
+        public float Range;
+        public float Damage;
+
+        public abstract bool Activate();
+
+        //All weapons will have some for of reload weather it be a cooldown
+        //or ammo depletion.
+        public abstract IEnumerator Reload();
+
+        //Implement Damage
+    }
+}
